7ffed7b2a796f7d6c89d6ad5096b95fa42fb22a7
at-luantran <luan.tran@asiantech.vn>
2018-07-31 10:15:17 +0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,8 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       flash[:success] = 'Login success'
       login(user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      remember(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
@@ -16,7 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 end
