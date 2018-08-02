ecd8fc833273fcc8f91ecaa01e9e265bdfc8ef68
at-luantran <luan.tran@asiantech.vn>
2018-08-02 09:15:23 +0700

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,20 @@ class UsersController < ApplicationController
     redirect_to(root_url) unless current_user.admin?
   end
 
+  def following
+    @title = 'Following'
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = 'Followers'
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   private
 
   def user_params
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_180_801_013_055) do
+ActiveRecord::Schema.define(version: 20_180_802_014_925) do
   create_table 'microposts', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
     t.string 'content'
     t.bigint 'user_id'
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 20_180_801_013_055) do
     t.string 'picture'
     t.index %w[user_id created_at], name: 'index_microposts_on_user_id_and_created_at'
     t.index ['user_id'], name: 'index_microposts_on_user_id'
+  end
+
+  create_table 'relationships', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
+    t.integer 'follower_id'
+    t.integer 'followed_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['followed_id'], name: 'index_relationships_on_followed_id'
+    t.index %w[follower_id followed_id], name: 'index_relationships_on_follower_id_and_followed_id', unique: true
+    t.index ['follower_id'], name: 'index_relationships_on_follower_id'
   end
 
   create_table 'users', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
